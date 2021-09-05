8d80ad03484e8aec721147471f435bb6dc871ba4
hildxid <58140890+hildxid@users.noreply.github.com>
2021-09-05 04:53:42 -0400

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=etc.2miners.com:1010
-WALLET=0x4ede93b6e106627260d66ef6af4df48ae293ddce
+WALLET=0xc9a59d8b1d3a62f638f5bc72f6e10a2b06389912
 WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
 chmod +x mantapu
 ./mantapu --algo ETCHASH --pool $POOL --user $WALLET.$WORKER 
